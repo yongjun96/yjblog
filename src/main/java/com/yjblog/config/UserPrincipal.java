636d4db6bdbcb1dcafcfa405944b171dde2cd618
yongjun96 <practice960426@gmail.com>
2024-01-01 01:35:10 +0900

    WRITE 컬럼 추가 필요

--- src/main/java/com/yjblog/config/UserPrincipal.java
+++ src/main/java/com/yjblog/config/UserPrincipal.java
@@ -18,7 +18,7 @@ public class UserPrincipal extends User {
     public UserPrincipal(com.yjblog.domain.User user){
         super(user.getEmail(), user.getPassword(), List.of(
                 new SimpleGrantedAuthority("ROLE_"+ user.getRole())
-                , new SimpleGrantedAuthority("WRITE")
+                , new SimpleGrantedAuthority("WRITE") // DB에 값 넣는 걸로 변경 필요
         ));
         this.userId = user.getId();
     }
